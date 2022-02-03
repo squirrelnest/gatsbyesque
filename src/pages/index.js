2f27e6e2ed112dc3bf1ae6f646b97306a1850fd4
Mikka <mikkasoft@gmail.com>
2022-02-03 12:49:27 -0800

    update text to trigger netlify bui;d

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Header from "../components/header"
 export default function Home() {
   return ( 
     <div> 
-      <Header headerText="Hello fuckface. This is a header."/>
+      <Header headerText="Hello there! This is a header."/>
       <p>Much wow. So React.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
